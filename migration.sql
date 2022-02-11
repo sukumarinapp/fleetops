9f4e14f6f0a46ea4d40811c8c26906a9216e9c46
sukumarinapp <sukumar.inapp@gmail.com>
2022-02-11 10:42:19 +0530

    new repo

--- migration.sql
+++ migration.sql
@@ -1,2 +1,12 @@
 alter table vehicle add password varchar(20) DEFAULT NULL;
 update vehicle set password = '1234';
+
+CREATE TABLE `driver_login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `VNO` varchar(20) DEFAULT NULL,
+  `driver_id` int(11) DEFAULT NULL,
+  `login_time` datetime DEFAULT NULL,
+  `otp` varchar(20) DEFAULT NULL,
+  `otp_verified` int(0) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4;	
