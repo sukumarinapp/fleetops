5e9b48325dd5ac18251b145c4700a16cf02f4966
sukumarinapp <sukumar.inapp@gmail.com>
2022-02-15 14:36:14 +0530

    new repo

--- migration.sql
+++ migration.sql
@@ -9,4 +9,9 @@ CREATE TABLE `driver_login` (
   `otp` varchar(20) DEFAULT NULL,
   `otp_verified` int(0) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;	
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+alter table vehicle add IEX date DEFAULT NULL;	
+alter table vehicle add REX date DEFAULT NULL; 
+alter table vehicle add AVI int(1) DEFAULT 0; 
+alter table vehicle add AVR int(1) DEFAULT 0; 
