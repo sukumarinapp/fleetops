5e598ed95b64b6f7291746f8dc85c712ad7a8c8a
universejino <universejino@gmail.com>
2022-03-07 12:42:40 +0530

    tasks screen

--- resources/views/driver/tasks.blade.php
+++ resources/views/driver/tasks.blade.php
@@ -11,21 +11,26 @@
 </div>
 <div class="card card-success">
   <div class="card-header">
-
+   @if($VBM == "Hire Purchase")
+   <h5 class="title" style="text-align:center;">Hire Purchase Customer</h5>
+   @elseif($VBM == "Rental")
+   <h5 class="title" style="text-align:center;">Vehicle Rental Customer</h5>
+   @else
    <h5 class="title" style="text-align:center;">Ride Hailing Driver</h5>
-  
+   @endif
 
  </div>
  <div class="card-body">
+  <input type="hidden" id="VNO" name="VNO" value="{{ $VNO }}">
   <div class="row justify-content-center">
     <div class="col-md-12">
-     <label class="col-form-label">Account Name   : </label>
+     <label class="col-form-label">Account Name   : </label>{{ $DNM }}
    </div>
    <div class="col-md-12">
-     <label class="col-form-label">Vehicle Reg No : </label>
+     <label class="col-form-label">Vehicle Reg No : </label>{{ $VNO }}
    </div>
    <div class="col-md-12">
-     <label class="col-form-label">Reg Phone Number : </label>
+     <label class="col-form-label">Reg Phone Number : </label>{{ $DCN }}
    </div>
  </div>
  <hr>
