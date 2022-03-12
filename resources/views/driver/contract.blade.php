0702fba2517c5cf31f672247b98cc692105f17b8
universejino <universejino@gmail.com>
2022-03-12 15:21:00 +0530

    licence upload screen

---
+++ resources/views/driver/contract.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.driver')
+@section('content')
+<div class="container" >
+ <div class="row justify-content-center">
+  <div class="col-md-12 text-center">
+    <a href="#" class="navbar-brand">
+      <img src="{{ URL::to('/') }}/images/fleetopslogo.png" alt="AdminLTE Logo">
+    </a>
+    <h3 style="color: lightgray">My Account</h3>
+  </div>
+</div>
+<div class="card card-success">
+  <div class="card-header">
+   <h5 class="title" style="text-align:center;">Vehicle Rental Customer</h5>
+
+ </div>
+ <div class="card-body">
+  <input type="hidden" id="VNO" name="VNO" value="">
+
+<div class="row">
+  <div class="col-md-12">
+    <embed src="../../uploads/VID/1.pdf" width="350" height="375" 
+ type="application/pdf">
+</div>
+</div>
+
+ <div class="row justify-content-center">
+   
+ </div>
+<nav class="navbar fixed-bottom navbar-expand-lg justify-content-center">    
+<a href="{{ url('tasks') }}/{{ Session::get('VNO') }}" class="btn btn-info">Back</a>&nbsp;  
+  <a href="{{ route('driver') }}" class="btn btn-info">Logout</a>
+</nav>
+</div>
+</div>
+</div>
+
+@endsection
