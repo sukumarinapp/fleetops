b1bae04d21568596ad85876c58459bd953f6b70a
sukumarinapp <sukumar.inapp@gmail.com>
2022-03-14 11:27:35 +0530

    add table

---
+++ resources/views/workbook.sql
@@ -0,0 +1,18 @@
+License Expiry 
+DLD
+LEX
+AVL if ON sms on last 3 days
+Insurance Expiry
+VID
+IEX
+AVI if ON sms on last 3 days
+RdWCert Expiry
+VRD
+REX
+AVR if ON sms on last 3 days 
+Contract Expiry
+VCC
+CEX
+AVC if ON sms on last 3 days
+Log and raise workflow tbl140 
+Log and sms task to driver
