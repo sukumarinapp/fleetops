c7be74a45ffdf9e5a9c39119b69595573c7f9d7a
sukumarinapp <sukumar.inapp@gmail.com>
2022-03-19 12:46:25 +0530

    expiry SMS

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -234,7 +234,7 @@ class DriverController extends Controller
     public function receipts()
     {
         $VNO = Session::get('VNO');
-        $sql = "select b.VMK,b.VMD,c.VBM,c.DNM,c.DSN,c.DCN from vehicle b,driver c where  b.driver_id=c.id and b.VNO='$VNO'";
+        $sql = "select b.VMK,b.VMD,c.VBM,c.DNM,c.DSN,c.DCN from tbl137 a,vehicle b,driver c where  a.VNO = b.VNO and b.driver_id=c.id and b.VNO='$VNO'";
         $result = DB::select(DB::raw($sql));
         if(count($result) > 0){
             $VBM = $result[0]->VBM;
