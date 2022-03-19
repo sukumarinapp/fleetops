ab14a82c2ec3fd6f99bb1e0b234bca0304ebf56a
universejino <universejino@gmail.com>
2022-03-19 16:08:59 +0530

    tasks

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -234,6 +234,15 @@ class DriverController extends Controller
     public function receipts()
     {
         $VNO = Session::get('VNO');
+            $VBM = "";
+            $DNM = "";
+            $VMK = "";
+            $DSN = "";
+            $DCN = "";
+            $VMD = "";
+            $SDT = "";
+            $RMT = "";
+            $VPF = "";
         $sql = "select a.SDT,a.RMT,a.RNO,b.VMK,b.VMD,c.VBM,c.DNM,c.DSN,c.DCN,c.VPF from tbl137 a,vehicle b,driver c where a.VNO = b.VNO and b.driver_id=c.id and b.VNO='$VNO' and a.VNO='$VNO' and RST = 1 order by SDT desc";
          $result = DB::select(DB::raw($sql));
          if(count($result) > 0){
@@ -246,7 +255,9 @@ class DriverController extends Controller
             $VPF = $result[0]->VPF;
   
        return view('driver.receipts',compact('result','VNO','VBM','DNM','DCN','VMK','SDT','RMT','VPF'));
-       }
+       }else{
+            return view('driver.receipts',compact('result','VNO','VBM','DNM','DSN','DCN','VMK','VMD','SDT','RMT','VPF'));;
+        }
     }
 
      public function uploadlicence()
