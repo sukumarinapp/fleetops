be2457c02059946da3e3c4628ffb099917bf4aa4
universejino <universejino@gmail.com>
2022-03-21 13:05:22 +0530

    tasks

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -213,7 +213,7 @@ class DriverController extends Controller
         $SPF = "";
         $TPF = "";
         $RMT = "";
-        $sql = "select a.SDT,a.RMT,a.RHN,a.SPF,a.TPF,b.VMK,b.VMD,c.VBM,c.DNM,c.DSN,c.DCN,c.VPF from tbl137 a,vehicle b,driver c where a.VNO = b.VNO and b.driver_id=c.id and b.VNO='$VNO' and a.VNO='$VNO' and RST = 1 order by SDT desc";
+        $sql = "select a.SDT,a.RMT,a.RHN,a.SPF,a.TPF,b.VMK,b.VMD,c.VBM,c.DNM,c.DSN,c.DCN,c.VPF from tbl137 a,vehicle b,driver c where a.VNO = b.VNO and b.driver_id=c.id and b.VNO='$VNO' and a.VNO='$VNO' and a.VBM='Ride Hailing' RST = 1 order by SDT desc";
         $result = DB::select(DB::raw($sql));
         if(count($result) > 0){
             $DNM = $result[0]->DNM . " " . $result[0]->DSN;
