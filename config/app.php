b8cb52bb60d638dc264582340fa8f1edf00a30ca
sukumarinapp <sukumar.inapp@gmail.com>
2022-04-18 17:53:22 +0530

    PWA

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Ladumor\LaravelPwa\PWAServiceProvider::class,
-
+        Barryvdh\DomPDF\ServiceProvider::class
     ],
 
     /*
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'LaravelPwa' => \Ladumor\LaravelPwa\LaravelPwa::class,
-
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
