cd60135f65eb8404f2aa224f8815a01c07ad5869
sukumarinapp <sukumar.inapp@gmail.com>
2022-04-18 18:33:54 +0530

    PWA

---
+++ resources/views/handoverpdf.blade.php
@@ -0,0 +1,12 @@
+<table>
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Address</th>
+    </tr>
+    <tr>
+        <td>34534543</td>
+        <td>43t54354</td>
+        <td>{43543543</td>
+    </tr>
+</table>
