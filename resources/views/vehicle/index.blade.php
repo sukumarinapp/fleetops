656a1c7e16974f4c0581b5074b3b25a7d4851295
sukumarinapp <sukumar.inapp@gmail.com>
2022-07-04 14:21:36 +0530

    unblock on payment

--- resources/views/vehicle/index.blade.php
+++ resources/views/vehicle/index.blade.php
@@ -243,7 +243,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel"><span><img src='/warning.png'></span> Alert</h5>
-            <h6 class="modal-title w-50 text-center" id="VNOPOP"></h6>
+            <h5 class="modal-title w-80 text-center" id="VNOPOP"></h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
