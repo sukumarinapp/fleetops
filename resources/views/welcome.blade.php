4d38f9db0e8bb4a28d07fe4856175b85c6c52601
sukumarinapp <sukumar.inapp@gmail.com>
2022-07-08 13:34:45 +0530

    unblock on payment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,13 +89,13 @@
                 </div>
             </div>
         </div>
-        <!-- <script src="{{ asset('/sw.js') }}"></script>
+        <script src="{{ asset('/sw.js') }}"></script>
         <script>
             if (!navigator.serviceWorker.controller) {
                 navigator.serviceWorker.register("/sw.js").then(function (reg) {
                     console.log("Service worker has been registered for scope: " + reg.scope);
                 });
             }
-        </script> -->
+        </script>
     </body>
 </html>
